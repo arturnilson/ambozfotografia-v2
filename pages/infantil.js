c95522a394b0cceac43cb6a221cecf65c6c44894
Artur Nilson <arturnilson@gmail.com>
2021-03-25 21:45:30 -0300

    Commit

--- pages/infantil.js
+++ pages/infantil.js
@@ -1,6 +1,37 @@
 const infantil = () => {
     return (
         <>
+            <div className="container-full container-fluid">
+                <h1 className="h1-events">Infantil</h1>
+                <hr />
+                <br />
+
+            </div>
+
+
+
+            {/* <Container fluid className="container-full">
+                <h1 className="h1-events">Infantil</h1>
+                <hr />
+                <br />
+                <SRLWrapper options={options}>
+                    <Row md="3" lg="3" sm="1">
+                        {projects && projects.map(img => (
+                            <Col lg="4" md="4" sm="auto">
+                                <motion.div className='col-image-small' key={img.id}
+                                    whileHover={{ opacity: 1 }}
+                                >
+                                    <motion.img className="image" src={img.photo} alt={img.text}
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        transition={{ delay: 1 }}
+                                    />
+                                </motion.div>
+                            </Col>
+                        ))}
+                    </Row>
+                </SRLWrapper>
+            </Container> */}
         </>
     )
 }
