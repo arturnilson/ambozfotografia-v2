5727955d79feb18877743f8702fc447a71de99d6
Artur Nilson <arturnilson@gmail.com>
2021-04-05 17:17:33 -0300

    Modificado texto do index e fonte do site

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,32 @@ import Container from 'react-bootstrap/Container';
 export default function Home() {
   return (
     <Container fluid className="container-fluid">
-      <p className="p-home"><span className="drop">L</span>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Etiam tempor orci eu lobortis elementum nibh. Massa sapien faucibus et molestie ac feugiat sed lectus. Nunc sed blandit libero volutpat sed cras ornare. Porta nibh venenatis cras sed. Fermentum iaculis eu non diam phasellus. Dui vivamus arcu felis bibendum ut tristique et egestas. Urna duis convallis convallis tellus id interdum velit laoreet id. Euismod nisi porta lorem mollis aliquam ut porttitor. Aliquam ut porttitor leo a diam sollicitudin tempor id eu. Pulvinar mattis nunc sed blandit. Aliquam sem et tortor consequat id porta nibh venenatis. Malesuada fames ac turpis egestas sed tempus.
-            <br /><br />
-        <span className="drop">F</span>aucibus purus in massa tempor nec. Faucibus scelerisque eleifend donec pretium vulputate sapien. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Amet facilisis magna etiam tempor orci eu lobortis elementum. Vel eros donec ac odio tempor orci dapibus ultrices. Sit amet facilisis magna etiam tempor orci eu lobortis. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi. Non tellus orci ac auctor. Adipiscing elit pellentesque habitant morbi tristique. Sed faucibus turpis in eu mi bibendum neque egestas congue. Tortor at risus viverra adipiscing at in tellus integer. Est ullamcorper eget nulla facilisi etiam dignissim diam. Vel eros donec ac odio tempor orci. Congue quisque egestas diam in arcu. Amet est placerat in egestas erat imperdiet sed. Feugiat pretium nibh ipsum consequat.
-            <br /><br />
-        <span className="drop">S</span>ed velit dignissim sodales ut eu sem integer vitae. Ultrices eros in cursus turpis massa tincidunt dui. Fermentum dui faucibus in ornare. Sit amet nisl suscipit adipiscing bibendum est. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.
+      <p className="p-home">
+        <h1 className="title">Amboz Fotografia</h1><br />
+        Duas pessoas que juntas encontraram na fotografia uma nova maneira de enxergar o mundo.
+        <br /><br />
+        Há 6 anos no mercado, construímos nossa identidade a partir da espontaneidade dos momentos verdadeiros.
+        <br /><br />
+        Somos especializados em registrar eventos, desde festas de aniversários, formaturas, casamentos e, também, eventos corporativos.
+        <br /><br />
+        Trabalhamos com ensaios fotográficos externos para pessoas e empresas, buscando sempre ressaltar as qualidades dos modelos, sejam profissionais ou momentâneos.
+        <br /><br />
+        Se quiser conhecer um pouco mais de quem somos e do que fazemos, acompanhe-nos nas redes sociais:
+        <br /><br /><br />
+
+        <div className="vl">
+          <a href="https://www.instagram.com/ambozfotografia" target="_blank">
+            @ambozfotografia
+          </a>
+          <br /><br />
+          <a href="https://www.instagram.com/mandicandradefotografa" target="_blank">
+            @mandicandradefotografa
+          </a>
+          <br /><br />
+          <a href="https://www.instagram.com/henriquestandtfoto" target="_blank">
+            @henriquestandtfoto
+          </a>
+        </div>
       </p>
     </Container>
   )
